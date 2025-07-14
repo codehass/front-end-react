103ee376fa83cf5b556a17b4a755f456b3f3f263
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-07-14 18:04:56 +0100

    create Task model

--- todoApp/backend/models/Task.js
+++ todoApp/backend/models/Task.js
@@ -10,6 +10,11 @@ const taskSchema = new mongoose.Schema(
 			type: Boolean,
 			default: false,
 		},
+		project: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Project",
+			required: true,
+		},
 	},
 	{ timestamps: true }
 );
