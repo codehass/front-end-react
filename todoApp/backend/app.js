94c750e2bec2f945a4508295fdcca4984bfab052
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-07-14 19:09:39 +0100

    add helmet and cors

--- todoApp/backend/app.js
+++ todoApp/backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
@@ -9,6 +11,8 @@ dotenv.config();
 
 connectDB();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
@@ -18,7 +22,8 @@ app.get("/", (req, res) => {
 	res.send("hello from ExpressJS Server!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
