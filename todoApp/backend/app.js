e69a08f72ae99c022a205271620ed3a2abb01ad5
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-07-14 12:15:35 +0100

    Install nodemon

--- todoApp/backend/app.js
+++ todoApp/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("hello from express!");
+	res.send("hello from ExpressJS Server!");
 });
 
 const PORT = 5000;
