6abe7e6215e56f9c039ae4c4e971380d81eb4dab
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-07-14 18:04:14 +0100

    Register routes

--- todoApp/backend/app.js
+++ todoApp/backend/app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const taskRoutes = require("./routes/taskRoutes");
+const projectRoutes = require("./routes/projectRoutes");
 
 dotenv.config();
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/projects", projectRoutes);
 
 app.get("/", (req, res) => {
 	res.send("hello from ExpressJS Server!");
